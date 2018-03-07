6fdd9aeb31ee948adf9c3012766bfa63a1c0be82
Klolensen <31515047+Klolensen@users.noreply.github.com>
2018-03-07 11:40:40 +0100

    Did the thing

---
+++ HttpStart/HttpCommandHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpStart
+{
+    class HttpCommandHandler
+    {
+        private TcpClient _client;
+        public HttpCommandHandler(TcpClient client)
+        {
+            _client = client;
+        }
+
+        public void HandleCommand()
+        {
+            while (true)
+            {
+                try
+                {
+                    Stream stream = _client.GetStream();
+                    StreamWriter streamWriter = new StreamWriter(stream) {AutoFlush = true};
+                    StreamReader streamReader = new StreamReader(stream);
+
+                    string message = streamReader.ReadLine();
+
+                    while (!string.IsNullOrEmpty(message))
+                    {
+                        Console.WriteLine(message);
+                        if (message == "STOP")
+                        {
+                            return;
+                        }
+                        List<string> messageList = message.Split(' ').ToList();
+                        foreach (string messageFragment in messageList)
+                        {
+                            Console.WriteLine(messageFragment);
+                        }
+                        message = "";
+                        messageList.Clear();
+                    }
+                }
+                catch (IOException)
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
--- HttpStart/Program.cs
+++ HttpStart/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,16 @@ namespace HttpStart
     {
         static void Main(string[] args)
         {
+            
+            TcpListener tcpListener = new TcpListener(IPAddress.Parse("10.200.123.158"), 6789);
+            tcpListener.Start();
+            Console.WriteLine("Server started");
+
+            while (true)
+            {
+                HttpCommandHandler handler = new HttpCommandHandler(tcpListener.AcceptTcpClient());
+                Task.Run(() => handler.HandleCommand());
+            }
         }
     }
 }
